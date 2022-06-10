5d9dbc6bd1e71ec47cb0965b96390e08576f06d7
@Jonathan.CASTILLO1 <jonathan.castillo@naranjax.com>
2022-06-10 16:56:40 -0300

    add connection to database

--- src/database/config.ts
+++ src/database/config.ts
@@ -2,19 +2,14 @@ import mongoose from 'mongoose';
 
 const dbConnection = async() =>{
     try {
-        await  mongoose.connect(process.env.MONGODB_CNN,{
-            useCreateIndex:true,
-            useFindAndModify:false,
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        })
-        console.log('Data Base online');
+        await  mongoose.connect(process.env.MONGODB_CNN || '')
+        console.log('Data Base online'.bgGreen);
     } catch (error) {
         console.log(error);
-        throw Error('Failed to connect database.')
+        throw Error('Failed to connect database.'.red)
     }
 };
 
-module.exports={
+export{
     dbConnection
 };
